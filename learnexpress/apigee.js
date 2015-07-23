40b3480bfa571b06659825050780d506e9596933
bzheng <bohan.zheng@am.sony.com>
2015-07-22 18:12:37 -0700

    lalal a

--- learnexpress/apigee.js
+++ learnexpress/apigee.js
@@ -7,10 +7,10 @@ var client = new Usergrid.client({
 });
 
 function getItems(args, callback) {
-	var options = {
-	    type: 'items',
-	    client: client
-	};
+    var options = {
+        type: 'items',
+        client: client
+    };
     var collection = new Usergrid.collection(options);
     collection.fetch(function(err, result) {
         if (err) {
@@ -22,56 +22,89 @@ function getItems(args, callback) {
     });
 }
 
-function saveItem(args, callback) {
-	args.type = 'items';
-	client.createEntity(args, function(err, result) {
-		if (err) {
-			Logger.logError(err);
+function getItemsByType(type, callback) {
+    var options = {
+        endpoint: "items",
+        qs: {
+            ql: "item_type = '" + type + "'"
+        }
+    };
+    client.request(options, function (err, result) {
+    	if (err) {
+    		Logger.logError(err);
             callback(err, result);
-		} else {
-			callback(err, result._data);
-		}
-	});
+    	} else {
+    		callback(err, result.entities);
+    	}
+    });
+}
+
+function getItemByUUid(uuid, callback) {
+    var options = {
+        type: "items",
+        uuid: uuid
+    };
+
+    client.getEntity(options, function(err, result) {
+        if (err) {
+            Logger.logError(err);
+            callback(err, result);
+        } else {
+            callback(err, result._data);
+        }
+    });
+}
+
+function saveItem(args, callback) {
+    args.type = 'items';
+    client.createEntity(args, function(err, result) {
+        if (err) {
+            Logger.logError(err);
+            callback(err, result);
+        } else {
+            callback(err, result._data);
+        }
+    });
 }
 
 function updateItem(args, callback) {
-	args.type = 'items';
-	var properties = {
-		client: client,
-		data: args
-	};
-	
-	var entity = new Usergrid.entity(properties);
-	
-	entity.save(function(err, result) {
-		if (err) {
-			Logger.logError(err);
+    args.type = 'items';
+    var properties = {
+        client: client,
+        data: args
+    };
+
+    var entity = new Usergrid.entity(properties);
+
+    entity.save(function(err, result) {
+        if (err) {
+            Logger.logError(err);
             callback(err, result);
-		} else {
-			callback(err, result.entities[0]);
-		}
-	});
+        } else {
+            callback(err, result.entities[0]);
+        }
+    });
 }
 
 function deleteItem(args, callback) {
-	var properties = {
-		client: client,
-		data: {
-			'type': 'items',
-			'uuid': args['uuid']
-		}
-	};
-	
-	var entity = new Usergrid.entity(properties);
-	
-	entity.destroy(function(err, result) {
-		if (err) {
-			Logger.logError(err);
-			callback(err, result);
-		} else {
-			callback(err, result.entities[0]);
-		}
-	});
+    var properties = {
+        client: client,
+        data: {
+            'type': 'items',
+            'uuid': args['uuid']
+        }
+    };
+
+    var entity = new Usergrid.entity(properties);
+
+    entity.destroy(function(err, result) {
+        if (err) {
+            Logger.logError(err);
+            callback(err, result);
+        } else {
+            callback(err, result.entities[0]);
+        }
+    });
 }
 
 function create(data, callback) {
@@ -125,12 +158,18 @@ module.exports = {
         getItems(args, callback);
     },
     'saveItem': function(args, callback) {
-    	saveItem(args, callback);
+        saveItem(args, callback);
     },
     'deleteItem': function(args, callback) {
-    	deleteItem(args, callback);
+        deleteItem(args, callback);
     },
     'updateItem': function(args, callback) {
-    	updateItem(args, callback);
+        updateItem(args, callback);
+    },
+    'getItemByUUid': function(args, callback) {
+        getItemByUUid(args, callback);
+    },
+    'getItemsByType': function(args, callback) {
+        getItemsByType(args, callback);
     }
 };
