39774d7985a1b17e3dd8360a5ab1493c8ef1091f
bzheng <bohan.zheng@am.sony.com>
2015-07-23 16:14:39 -0700

    asdf

--- learnexpress/app.js
+++ learnexpress/app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var apigee = require('./apigee.js');
 var resultFactory = require('./result_factory.js');
+//config port
+var port = process.env.PORT || 3000;
+var router = require('./router.js');
 
 var app = express();
 
@@ -16,76 +19,6 @@ app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
     next('route');
-});
-
-//config port
-var port = process.env.PORT || 3000;
-
-//Router level middleware
-var router = express.Router();
-
-/**
- * operations to all items
- */
-
-router.route('/items').get(function(req, res) {
-    //get a list of items
-    apigee.getItems({}, function(err, result) {
-        res.json(resultFactory.create(err, result));
-    });
-}).post(function(req, res) {
-    //add a new item
-    var data = req.body;
-
-    apigee.saveItem(data, function(err, result) {
-        res.json(resultFactory.create(err, result));
-    });
-});
-
-/**
- * operations to a given type of items
- */
-
-router.route('/items/:itemType').get(function(req, res) {
-    //get a list items for a certain type
-    apigee.getItemsByType(req.params.itemType, function(err, result) {
-        res.json(resultFactory.create(err, result));
-    });
-}).post(function(req, res) {
-    //add a new item to a certain type
-    var data = req.body;
-    data.itemType = req.params.itemType;
-    apigee.create(data, function(err) {
-        var result = resultFactory.create(err);
-        res.json(result);
-    });
-});
-
-/**
- * operations to a given item identified by uuid
- */
-
-router.route('/item/:uuid').get(function(req, res) {
-    //get an item by id
-    apigee.getItemByUUid(req.params.uuid, function(err, result) {
-        res.json(resultFactory.create(err, result));
-    });
-}).put(function(req, res) {
-    //update an item by id
-    var data = req.body;
-    console.log("log in app.js/item/:uuid/put ---> \n" + data);
-    data.uuid = req.params.uuid;
-    apigee.updateItem(data, function(err, result) {
-        res.json(resultFactory.create(err, result));
-    });
-}).delete(function(req, res) {
-    //delete an item by id
-    var uuid = req.params.uuid;
-    apigee.deleteItem({
-        'uuid': uuid
-    }, function(err, result) {
-        res.json(resultFactory.create(err, result));
-    });
 });
 
 app.use('/myapi', router);
